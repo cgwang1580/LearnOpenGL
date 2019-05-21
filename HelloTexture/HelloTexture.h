ac2691b825f1701e4f85957e7a8f1556c9deb690
Chauncy <cgwang1580@gmail.com>
2019-05-21 10:48:47 +0800

    add HelloTexture, show one texture

---
+++ HelloTexture/HelloTexture.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include <string>
+#include <Shader_Helper.h>
+
+const unsigned int WIN_WINDTH = 800;
+const unsigned int WIN_HEIGHT = 600;
+
+#define MY_MAX_PATH	256
+#define USE_SHADER_HELPER
+
+enum MERR_CODE {
+	MOK = 0,
+	MERR_INVALID_PARAMETER,
+	MERR_BAD_STATE,
+	MERR_NO_MEMORY
+};
+
+const string imagePath = "..\\Resource\\HelloTexture\\container.jpg";
+const string imagePath2 = "..\\Resource\\HelloTexture\\awesomeface.png";
+
+unsigned int texture;
+unsigned int texture2;
+unsigned int VAO;
+unsigned int VBO;
+unsigned int EBO;
+float vertices[] = {
+	// vertex				color				texture
+	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+};
+
+unsigned int indecies[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+const string vertexShaderPath = "Shader/vertex.shader";
+const string fragmentShaderPath = "Shader/fragment.shader";
+
+GLFWwindow* initGLFW();
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window);
