89a0c7cbe0d99d45783bb6374b0d0174e6127cd3
Chauncy <cgwang1580@gmail.com>
2019-06-07 15:54:32 +0800

    Add HelloCamera

---
+++ HelloCamera/HelloCamera.cpp
@@ -0,0 +1,217 @@
+#include "HelloCamera.h"
+
+int main() {
+
+	window = initGLFW();
+	if (NULL == window) {
+		cout << "initGLFW failed" << endl;
+		return -1;
+	}
+
+#ifdef USE_SHADER_HELPER
+	Shader_Helper shaderHelper(vertexShaderPath, fragmentShaderPath);
+#endif // USE_SHADER_HELPER
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_cube), vertices_cube, GL_STATIC_DRAW);
+
+	/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), indecies, GL_STATIC_DRAW);*/
+
+	// set vertex attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// set color attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// set texture attribute
+	/*glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);*/
+
+	glBindVertexArray(0);  // Unbind VAO
+
+						   // load image, create texture
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set texture proterty
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// read image
+	int imgWidth = 0;
+	int imgHeight = 0;
+	int nChannels = 0;
+	unsigned char* data = NULL;
+	stbi_set_flip_vertically_on_load(true);
+	data = stbi_load(imagePath.c_str(), &imgWidth, &imgHeight, &nChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgWidth, imgHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "stbi_load image failed" << endl;
+		return -1;
+	}
+	stbi_image_free(data);
+	glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+
+									 // load texture2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int imgWidth2 = 0;
+	int imgHeight2 = 0;
+	int nChannels2 = 0;
+	unsigned char* data2 = NULL;
+	stbi_set_flip_vertically_on_load(true);
+	data2 = stbi_load(imagePath3.c_str(), &imgWidth2, &imgHeight2, &nChannels2, 0);
+	if (data2) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgWidth2, imgHeight2, 0, GL_RGB, GL_UNSIGNED_BYTE, data2);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		cout << "stbi_load image2 failed" << endl;
+		return -1;
+	}
+	stbi_image_free(data2);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glEnable(GL_DEPTH_TEST);
+
+#ifdef USE_SHADER_HELPER
+	shaderHelper.use();
+	shaderHelper.setInt("texture1", 0);
+	shaderHelper.setInt("texture2", 1);
+#endif // USE_SHADER_HELPER
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		// active texture
+		glActiveTexture(GL_TEXTURE0);
+		// bind Texture
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		// Create transformations
+		// make sure to initialize matrix to identity matrix first, important!!!
+		glm::mat4 transform = glm::mat4(1.0f);
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+
+		/*glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));*/
+		GLfloat radius = 10.0f;
+		GLfloat camX = sin(glfwGetTime()) * radius;
+		GLfloat camY = cos(glfwGetTime()) * radius;
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::lookAt(glm::vec3(camX, 0.0f, camY), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0, 1.0, 0.0));
+
+		glm::mat4 perspective = glm::mat4(1.0f);
+		perspective = glm::perspective(45.0f, (float)WIN_WINDTH / WIN_HEIGHT, 0.1f, 100.0f);
+
+		// Get matrix's uniform location and set matrix
+		GLint transformLoc = glGetUniformLocation(shaderHelper.progreamId, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+		GLint viewLoc = glGetUniformLocation(shaderHelper.progreamId, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+		GLint perspectiveLoc = glGetUniformLocation(shaderHelper.progreamId, "projection");
+		glUniformMatrix4fv(perspectiveLoc, 1, GL_FALSE, glm::value_ptr(perspective));
+
+		glBindVertexArray(VAO);
+
+		for (int i = 0; i < 10; ++i) {
+			glm::mat4 model = glm::mat4(1.0);
+			model = glm::translate(model, cubePositions[i]);
+			GLfloat angel = 20.0f * i;
+			if (i%4 == 0) {
+				angel = (GLfloat)glfwGetTime();
+			}
+			model = glm::rotate(model, angel, glm::vec3(1.0f, 0.3f, 0.5f));
+			glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model));
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		//glBindVertexArray(0);
+
+		/*glm::mat4 scaleTransform = glm::mat4(1.0f);
+		float sinAngle = glm::abs(glm::sin((GLfloat)glfwGetTime()));
+		scaleTransform = glm::translate(scaleTransform, glm::vec3(-0.5f, 0.5f, 0));
+		scaleTransform = glm::scale(scaleTransform, glm::vec3(sinAngle, sinAngle, 1.0f));
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(scaleTransform));
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+		glBindVertexArray(0);
+
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &EBO);
+
+	glfwTerminate();
+
+	return 0;
+}
+
+GLFWwindow* initGLFW() {
+	if (!glfwInit()) {
+		return NULL;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIN_WINDTH, WIN_HEIGHT, "HelloCamera", NULL, NULL);
+	if (NULL == window) {
+		cout << "glfwCreateWindow failed" << endl;
+		glfwTerminate();
+		return NULL;
+	}
+	glfwMakeContextCurrent(window);
+	// register callback function
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to intialize GLAD" << endl;
+		return NULL;
+	}
+
+	int nrAttributes = 0;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	cout << "GL_MAX_VERTEX_ATTRIBS " << nrAttributes << endl;
+
+	return window;
+}
+
+void processInput(GLFWwindow *window) {
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// get input 
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
---
+++ HelloCamera/HelloCamera.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include <string>
+#include <Shader_Helper.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+const unsigned int WIN_WINDTH = 800;
+const unsigned int WIN_HEIGHT = 600;
+
+#define MY_MAX_PATH	256
+#define USE_SHADER_HELPER
+
+enum MERR_CODE {
+	MOK = 0,
+	MERR_INVALID_PARAMETER,
+	MERR_BAD_STATE,
+	MERR_NO_MEMORY
+};
+
+const string imagePath = "..\\Resource\\HelloTexture\\container.jpg";
+const string imagePath2 = "..\\Resource\\HelloTexture\\awesomeface.png";
+const string imagePath3 = "..\\Resource\\HelloTexture\\face.jpg";
+
+unsigned int texture;
+unsigned int texture2;
+unsigned int VAO;
+unsigned int VBO;
+unsigned int EBO;
+
+float thresh = 0.5;
+
+float vertices[] = {
+	// vertex				texture
+	 0.5f,   0.5f, 0.0f,   1.0f, 1.0f,
+	 0.5f,  -0.5f, 0.0f,   1.0f, 0.0f,
+	-0.5f,  -0.5f, 0.0f,   0.0f, 0.0f,
+	-0.5f,   0.5f, 0.0f,   0.0f, 1.0f
+};
+
+unsigned int indecies[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+float vertices_cube[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
+const string vertexShaderPath = "Shader/vertex.shader";
+const string fragmentShaderPath = "Shader/fragment.shader";
+GLFWwindow *window = NULL;
+
+GLFWwindow* initGLFW();
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window);
+
